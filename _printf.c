161beb31713579a6783a053d6ec74aebeabec961
silvioukoth <silvioukoth@gmail.com>
2022-08-22 00:42:57 -0700

    add task printf

--- _printf.c
+++ _printf.c
@@ -7,17 +7,11 @@ void print_buffer(char buffer[], int *buff_ind);
 
 
 /**
-
  * _printf - Printf function
-
  * @format: format.
-
  * Return: Printed chars.
-
  */
-
 int _printf(const char *format, ...)
-
 {
 
 	int i, printed = 0, printed_chars = 0;
@@ -107,19 +101,12 @@ int _printf(const char *format, ...)
 
 
 /**
-
  * print_buffer - Prints the contents of the buffer if it exist
-
  * @buffer: Array of chars
-
  * @buff_ind: Index at which to add next char, represents the length.
-
  */
-
 void print_buffer(char buffer[], int *buff_ind)
-
 {
-
 	if (*buff_ind > 0)
 
 		write(1, &buffer[0], *buff_ind);
